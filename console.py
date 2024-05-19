2530fce3e6d4a37f1cba1510df5a358d7d3930a8
Makenalyn <makenalynton@gmail.com>
2024-05-19 06:49:24 -0700

    added EOF, quit, help has it as default

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" the console of the AirBnB"""
+import cmd
+
+
+"""
+hbnb class importing the public Cmd class
+"""
+
+
+class hbnb(cmd.Cmd):
+    def do_EOF(self, line):
+        return True
+
+    def do_quit(self, line):
+        return True
+
+
+if __name__ == "__main__":
+    hbnb().cmdloop()
