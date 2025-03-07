b6112576be14b0b39ec82698cdf56359109e1d4f
Hingamakena <hingamakena@gmail.com>
2025-03-07 10:33:06 -0800

    returns self.__dict__, returns class.__name__

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
 import uuid
+import time
 from datetime import datetime
 
-"""
-class model defining all common attributes/methods for other classes
-"""
-
-
 class BaseModel:
-
-    """ the init class method """
-    def __init__(self, *args, **kwargs):
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-                if 'created_at' in kwargs:
-                    self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%")
-                if 'updated_at' in kwargs:
-                    self.updated_at = datetime.strptime(kwatgs["updated_at"], "%Y-%m-%dT%")
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
+    id = str(uuid.uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        print("{}".format(BaseModel.__name__))
 
     def save(self):
-        self.updated_at = datetime.now()
+        updated_at = datetime.now()
+
 
     def to_dict(self):
-        object_dict = self.__dict__.copy()
-        object_dict['__class__'] = self.__class__.__name__
-        object_dict['updated_at'] = self.updated_at.isoformat()
-        object_dict['created_at'] = self.created_at.isoformat()
-        return object_dict
+        return self.__dict__
+
+if __name__ == "__main__":
+    a = BaseModel()
+    a.__str__
