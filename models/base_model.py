6d01b6995826f98ed6eab0d0f21ad6b2438a4c18
Hingamakena <hingamakena@gmail.com>
2025-03-08 11:11:52 -0800

    updated the print to return

--- models/base_model.py
+++ models/base_model.py
@@ -14,9 +14,8 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-
     def __str__(self):
-        print("{} {} {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return f"{self.__class__.__name__} {self.id} {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
