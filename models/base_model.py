dce564cd6d85446187752889c41d53d6d546f3cc
Hingamakena <hingamakena@gmail.com>
2025-03-08 11:09:48 -0800

    added the publci methods

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,16 @@ class BaseModel:
 
     """ pyblic instance attributes """
     def __init__(self):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
 
     def __str__(self):
         print("{} {} {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return f"{self.__class__.__dict__}"
