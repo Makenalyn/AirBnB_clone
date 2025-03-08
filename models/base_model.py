2ed66cc12b9a788f6d7ab63b031688cb59946a19
Hingamakena <hingamakena@gmail.com>
2025-03-08 10:48:17 -0800

    modified the class model. contains the public instances only

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
+
 import uuid
-import time
 from datetime import datetime
+
+""" A baseModel class which is the base of all other classes"""
+
 
 class BaseModel:
 
-
-    def __init__(self, *args, **kwargs):
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                print("{} {}".format(key, value))
-
-
-    id = str(uuid.uuid4())
+    """ pyblic instance attributes """
+    id = uuid.uuid4()
     created_at = datetime.now()
     updated_at = datetime.now()
-
-    def __str__(self):
-        print("{} {} {}".format(BaseModel.__name__ , self.id , self.__dict__))
-
-    def save(self):
-        updated_at = datetime.now()
-
-
-    def to_dict(self):
-        return self.__class__.__dict__
-
-if __name__ == "__main__":
-    a = BaseModel()
-    a.__str__
