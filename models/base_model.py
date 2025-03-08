47f762ab81295cc55e37e39eea1407525f17fe45
Hingamakena <hingamakena@gmail.com>
2025-03-08 10:58:54 -0800

    modified the pblic instances

--- models/base_model.py
+++ models/base_model.py
@@ -11,8 +11,8 @@ class BaseModel:
     """ pyblic instance attributes """
     def __init__(self):
         self.id = uuid.uuid4()
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
 
     def __str__(self):
