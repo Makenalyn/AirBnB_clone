42f41cd8a38c5dca7519efd81b056dd3624cee42
Hingamakena <hingamakena@gmail.com>
2025-04-01 23:03:05 -0700

    inclusive of kwargs and args parameters

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,15 @@ from datetime import datetime
 class BaseModel:
     """ public instance attributes """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        mydict = self.__dict__.copy()
+        if kwargs:
+            for i, j in mydict.items():
+                print(i, j)
 
     def __str__(self):
 
