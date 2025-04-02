f10e172cb6bf903e38f44ab1f273ec8d928c6fcc
Hingamakena <hingamakena@gmail.com>
2025-04-01 22:43:31 -0700

    corrected the self.updated_at in save module

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import uuid
+import datetime
 from datetime import datetime
 
 """ A baseModel class which is the base of all other classes"""
@@ -31,4 +32,8 @@ class BaseModel:
 
 if __name__ == "__main__":
     a = BaseModel()
-    print(a.save())
+    a.name = "My first model"
+    a.num = 89
+    print(a)
+    a.save()
+    print(a)
