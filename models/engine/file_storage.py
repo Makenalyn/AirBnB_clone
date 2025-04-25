c00d181e8644f2fe4929eec1c54fc7d78d71e930
Hingamakena <hingamakena@gmail.com>
2025-04-24 23:11:00 -0700

    attempt to file_storage'

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
 
-class Filestorage:
-    __file_path = "path"
-    __objects = 0
+""" class Filestorage that serializes instances to JSON file """
 
+
+class FileStorage:
+
+    """ private class attributes """
+    def __init__(self):
+        self.__file_path = ""
+        self.__objects = [self.__class__.__name__.id]
+
+    """ return the dictionary __objects """
     def all(self):
-        return __objects.__dict__
+        return self.__dict__.__objects
 
+    """ sets in __objects the obj with key <obj classname>.id """
     def new(self, obj):
-        pass
+        for key, value in __objects.items():
+            key['obj'] = ob.__class__.__name__.id
 
+    """ serializes __objects to the JSON file(path: __file_path) """
     def save(self):
-        return json.dumps(self)
+        with open(__file_path, 'r+') as f:
+            f.write(__objects.__dict__)
 
+    """ deserializes the JSON file to __objects( only if the path exist
+        no exception should be raised"""
     def reload(self):
-        return json.dumps(self)
+        x = json.loads(__objects.__dict__)
