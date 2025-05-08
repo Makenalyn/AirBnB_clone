d99e5f1d0ec0fe8ac61de3db2606611c9f8d3af9
Hingamakena <hingamakena@gmail.com>
2025-05-08 09:32:08 -0700

    imported variable storage, called save() method of storage , new() instance if not from dictionary rep

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 import uuid
 import datetime
 from datetime import datetime
-
+import storage
 """ A baseModel class which is the base of all other classes"""
 
 
@@ -16,8 +16,11 @@ class BaseModel:
 
         if kwargs:
             for key, value in kwargs.items():
-                key['created_at'] = datetime.fromisoformat(self.created_at)
-                key['updated_at'] = datetime.fromisoformat(self.updated_at)
+                key['created_at'] = datetime.fromisoformat(str(self.created_at))
+                key['updated_at'] = datetime.fromisoformat(str(self.updated_at))
+
+            if not isinstance(BaseModel.__dict__):
+                storage.new()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -29,6 +32,7 @@ class BaseModel:
     """ updates the public instance updated_at with the current datetime """
     def save(self):
         self.updated_at = datetime.now()
+        storage.reload()
 
     """ return the dictionary copy of the class instance """
     def to_dict(self):
