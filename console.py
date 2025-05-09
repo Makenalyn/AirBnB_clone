37af17ae15507b60e7dbd8f5eeb22576efad91dd
Hingamakena <hingamakena@gmail.com>
2025-05-09 12:05:09 -0700

    methods with description, not implemented

--- console.py
+++ console.py
@@ -18,6 +18,30 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         return True
 
+    def do_create(self, arg):
+        new_instance = BaseModel()
+        new_instance.save()
+        print(new_instance.id)
+
+        """ if class name is missing, print class name missing """
+        """ if doesn't exist, create MyModel """
+
+    def do_show(self, arg):
+        """ print string rep of an instance  based on the class name and id"""
+
+    def do_destroy(self, arg):
+        """ Delete an instance based on the class name and id """
+
+    def do_all(self, arg):
+        """ prints all string representation of all instances based or not on
+            the class name """
+
+    def do_update(self, arg):
+        """ update instance based on the class name and id by adding or updating
+            attribute"""
+
+
+
     """ File must end with """
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
